72c7f201f218416716f9748c3dbb0022a2e8476d
MathieuMolina <molina.mathieu@hotmail.com>
2022-03-23 11:09:31 +0100

    end

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 let tableau = ["banane","framboise","pomme","fraise","poire"];
 
+
 for(let i = 0; i < tableau.length; i++){
     console.log(tableau[i]);
+    document.body.innerHTML += tableau[i]+" "; //Faire apparaitre en HTML chaque element du tableau (et rajout d'un espace)
 }
 
 
-document.write(tableau.join());
+
+// // document.body.innerHTML = fruits
+// innerHTML
+// innerText
+// textContent
